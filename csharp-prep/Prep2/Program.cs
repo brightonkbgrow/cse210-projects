afec799777c704561e3fc56a562d98bb008bc1e0
brightongrow <brightongrow@byui.edu>
2024-09-18 14:48:24 -0600

    final

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -6,40 +6,41 @@ class Program
     {
         Console.Write("What did you score? ");
         string input = Console.ReadLine();
-        int score = int.Parse(input);
+        if (int.TryParse(input, out int score))
+        {
         string letter;
-                if (score >= 90)
+
+            if (score >= 90)
+            {
+                letter = "A";
+            }
+            else if (score >= 80)
+            {
+                letter = "B";
+            }
+            else if (score >= 70)
+            {
+                letter = "C";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+        if (score >= 70)
         {
-            letter = ("A");
-        }
-        else if (score >= 80)
-        {
-            letter = ("B");
-        }
-        else if (score >= 70)
-        {
-            letter = ("C");
+            Console.WriteLine($"Your grade is {letter}. You Passed!");
         }
         else if (score < 70)
         {
-            letter = ("F");
-        }
-        if (score >= 90)
-        {
-            Console.WriteLine($"Your grade is {letter}. You Passed!");
-        }
-        else if (score >= 80)
-        {
-            Console.WriteLine($"Your grade is {letter}. You Passed!");
-        }
-        else if (score >= 70)
-        {
-            Console.WriteLine($"Your grade is {letter}. You Passed!");
-        }
-        else
-        {
             Console.WriteLine($"Your grade is {letter}. You failed.");
         }
+    else
+    {
+        Console.WriteLine("Invalid input. Please enter a valid score.");
+    }
 
+
+        }
     }
 }
