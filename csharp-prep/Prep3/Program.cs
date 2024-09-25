7fd9ffb90f8d8ac17b6566532ff0b522f2a5809c
brightongrow <brightongrow@byui.edu>
2024-09-25 11:17:45 -0600

    finish code with polish

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        Console.Write("Guess my magic number! It's between 1 and 100. ");
+        int guess;
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out guess) && guess >= 1 && guess <= 100)
+            {
+                if (guess == number)
+                {
+                    Console.WriteLine("You Guessed Right.");
+                    break;
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Too High.");
+                }
+                else
+                {
+                    Console.WriteLine("Too Low.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("That isnt even a valid option. only numbers between 1 and 100.");
+            }
+
+            Console.Write("Guess Again: ");
+        }
     }
 }
