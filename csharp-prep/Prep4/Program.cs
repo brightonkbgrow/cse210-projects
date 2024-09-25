b3135880447bc6057ab3ced2b28e14fa9cf03e21
brightongrow <brightongrow@byui.edu>
2024-09-25 14:57:54 -0600

    finished

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    {   
+        List<double> x = new();
+        double input = -1;
+        Console.WriteLine("Enter numbers and press enter. then enter 0 to stop.");
+        while (input != 0)
+        {
+            Console.Write("Enter Number: ");
+            
+            string input_string = Console.ReadLine();
+            input = double.Parse(input_string);
+            if (input != 0)
+            {
+                x.Add(input);
+            }
+        }
+        double sum = Queryable.Sum(x.AsQueryable());
+        Console.WriteLine($"The Sum is {sum}");
+
+        double avg = Queryable.Average(x.AsQueryable());
+        Console.WriteLine($"The Average is {avg}");
+
+        double max = Queryable.Max(x.AsQueryable());
+        Console.WriteLine($"The Largest number is {max}");
     }
 }
