9c1704e0161fc4c9097cbc99140775bf1cba2ccb
brightongrow <brightongrow@byui.edu>
2024-10-07 13:25:51 -0600

    who knows

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        string navinput = "";
+        while (navinput != "exit");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Save");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Read");
+            Console.WriteLine("5. Exit");
+
+            Console.ReadLine();
+            
+        
     }
 }
---
+++ prove/Develop02/promptgen.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class PromptGenerator
+{
+    // Attributes
+    private string _list; // or you can name it public string List;
+
+    // Behaviors
+    public void ChoosePrompt()
+    {
+        Console.WriteLine("prompt");
+    }
+}
