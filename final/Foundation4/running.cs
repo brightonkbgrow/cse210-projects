38343aa821c469841829442f8629b6a6bfaadbeb
brightongrow <brightongrow@byui.edu>
2024-12-14 13:46:23 -0700

    foundation 4

---
+++ final/Foundation4/running.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Running : Activity
+{
+    private double _distance;
+
+    public Running(DateTime date, int minutes, double distance) : base(date, minutes)
+    {
+        _distance = distance;
+    }
+
+    public override double GetDistance() => _distance;
+
+    public override double GetSpeed() => GetDistance() / (Minutes / 60.0);
+
+    public override double GetPace() => Minutes / GetDistance();
+}
---
+++ final/Foundation4/swimming.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Swimming : Activity
+{
+    private int _laps;
+    private const double LapLengthKilometers = 50.0 / 1000.0;
+    private const double LapLengthMiles = LapLengthKilometers * 0.62;
+
+    public Swimming(DateTime date, int minutes, int laps) : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance() => _laps * LapLengthMiles;
+
+    public double GetDistanceKilometers() => _laps * LapLengthKilometers;
+
+    public override double GetSpeed() => GetDistance() / (Minutes / 60.0);
+
+    public double GetSpeedKilometersPerHour() => GetDistanceKilometers() / (Minutes / 60.0);
+
+    public override double GetPace() => Minutes / GetDistance();
+
+    public double GetPaceKilometers() => Minutes / GetDistanceKilometers();
+}
